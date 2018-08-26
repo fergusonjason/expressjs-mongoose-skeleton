c2b50fabfde3e027b25d76b9bade15ee7416a442
Jason <fergusonjason@gmail.com>
2018-08-26 17:42:07 -0400

    remove the actual authentication stuff since it was refactored into a passport middleware. Comments. Pull in the secret key from the config

--- components/auth/AuthenticationController.js
+++ components/auth/AuthenticationController.js
@@ -1,35 +1,35 @@
+import jwt from "jsonwebtoken";
+import config from 'config';
+import _ from "lodash";
+
 import User from './../user/User';
 import {log} from "./../../util/logger";
-import _ from "lodash";
-import jwt from "jsonwebtoken";
 
 let AuthenticationController = {};
+
+const secret = config.get('passport').secretKey;
 
 AuthenticationController.authenticate = async (req, res) => {
 
     log.info(`Entered AuthenticationController.authenticate(), email: ${req.body.email}`);
 
     try {
+        // use await so that I can unit test without losing my freaking
+        // mind
         var user = await User.findOne( {email: req.body.email}).exec();
-        if (user == null || user == {}) {
-            res.status(200).json({});
-        } else {
-            // we found the user, now check the password
-            if (user.comparePassword(req.body.password, user.password) == true) {
 
-                let fixedUser = _.omit(user, "password");
-                let token = jwt.sign(fixedUser.toJSON(), "RedRobinYummm");
-                res.status(200).json({success: true, token: `JWT ${token}`});
+        // I don't want the returned token to return the password, even
+        // if it is hashed, so let lodash omit that key
+        let fixedUser = _.omit(user, "password");
+        let token = jwt.sign(fixedUser.toJSON(), secret);
+        res.status(200).json({success: true, token: `JWT ${token}`});
 
-            } else {
-                // if it failed, send a 404 to imply the endpoint doesn't exist
-                res.status(404).send();
-            }
-            
-        }
     } catch (err) {
-        res.status(401).json({success: false});
+
+        // if it failed, send a 404 to imply the endpoint doesn't exist
+        res.status(404).send();
     }
+
 }
 
 export default AuthenticationController;
